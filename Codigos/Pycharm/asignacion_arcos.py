1db985d1861f815a417322a146a594e6c82a0e33
jpmerlet <jpdonosomerlet@gmail.com>
2018-10-02 11:00:31 -0300

    actualizacion de notebook y .py

--- Codigos/Pycharm/asignacion_arcos.py
+++ Codigos/Pycharm/asignacion_arcos.py
@@ -73,7 +73,7 @@ print('zdim: ', zdim)
 # dados los angulos theta en las
 # direcciones principales. Como primer
 # ejercicio se consideraron todos iguales
-theta_n = np.pi / 4
+theta_n = np.pi / 6
 theta_s = np.pi / 6
 theta_w = np.pi / 6
 theta_e = np.pi / 6
@@ -117,12 +117,63 @@ for k in range(1, numz + 1):
                         if Value <= 1:
                             arcos.append([(i, j, k), (m, n, k - t)])
 
-bloque_base = (3, 2, 3)  # bloque base para plotear
+# Lo mismo de antes, pero solo predeccesor inmediato
+S = {}
+# inicializamos S para el primer nivel
+for i in range(1, numx + 1):
+    for j in range(1, numy + 1):
+        S[(i, j, 1)] = []
+for k in range(1, numz + 1):
+    for i in range(1, numx + 1):
+        for j in range(1, numy + 1):
+            lista_predecesores = []
+            for t in range(1, k):
+                # dx1 = (k-t)*zdim/np.tan(theta_w)
+                # dy1 = (k-t)*zdim/np.tan(theta_s)
+                # dx2 = (k-t)*zdim/np.tan(theta_e)
+                # dy2 = (k-t)*zdim/np.tan(theta_n)
+
+                dx1 = t * zdim / np.tan(theta_w)
+                dy1 = t * zdim / np.tan(theta_s)
+                dx2 = t * zdim / np.tan(theta_e)
+                dy2 = t * zdim / np.tan(theta_n)
+
+                m1 = dx1 / xdim
+                n1 = dy1 / ydim
+                m2 = dx2 / xdim
+                n2 = dy2 / ydim
+                print('###################################')
+                print('nivel t=', t, '.bloque base:', (i, j, k),
+                      '.semi-ejes: dx1 = %.2f, dy1 = %.2f, dx2 = %.2f, dy2 = %.2f' % (dx1, dy1, dx2, dy2))
+                print('Parametros de las cantidades de bloques: m1=%.2f, n1=%.2f, m2=%.2f, n2=%.2f' % (m1, n1, m2, n2))
+                for m in range(max(math.floor(i - m2), 1), min(numx, math.ceil(i + m1)) + 1):
+                    for n in range(max(math.floor(j - n2), 1), min(numy, math.ceil(j + n1)) + 1):
+                        if m == i and n == j and t > 1:
+                            continue
+                        a = xdim * (i - m)
+                        b = ydim * (j - n)
+                        print('\n*********** Distancia del bloque X_{%.2f,%.2f,%.f} ***********' % (m, n, k - t))
+                        print('a=%.2f, b=%.2f' % (a, b))
+                        a2 = a ** 2
+                        b2 = b ** 2
+                        if m >= i and n >= j:
+                            Value = a2 / (dx1 ** 2) + b2 / (dy1 ** 2)
+                        elif m >= i and n <= j:
+                            Value = a2 / (dx1 ** 2) + b2 / (dy2 ** 2)
+                        elif m <= i and n <= j:
+                            Value = a2 / (dx2 ** 2) + b2 / (dy2 ** 2)
+                        else:
+                            Value = a2 / (dx2 ** 2) + b2 / (dy1 ** 2)
+                        if Value <= 1:
+                            lista_predecesores.append((m, n, k - t))
+            S[(i, j, k)] = lista_predecesores
+
+bloque_base_1 = (2, 3, 2)  # bloque base para plotear
 sacar = []
 for arco in arcos:
     bloque_tail = arco[0]
     bloque_head = arco[1]
-    if bloque_base == bloque_tail:
+    if bloque_base_1 == bloque_tail:
         sacar.append(bloque_head)
 print(sacar)
 
@@ -139,11 +190,25 @@ ax = fig.add_subplot(111, projection='3d')
 plt.gca().invert_zaxis()
 
 for v in sacar:
-    a = Arrow3D([(bloque_base[0]-1)*20, (v[0]-1)*20], [(bloque_base[1]-1)*20, (v[1]-1)*20],
-                [(bloque_base[2]-1)*15, (v[2]-1)*15], mutation_scale=20,
+    a = Arrow3D([(bloque_base_1[0]-1)*20, (v[0]-1)*20], [(bloque_base_1[1]-1)*20, (v[1]-1)*20],
+                [(bloque_base_1[2]-1)*15, (v[2]-1)*15], mutation_scale=20,
                 lw=3, arrowstyle="-|>", color="r")
     ax.add_artist(a)
 ax.scatter(ejex, ejey, ejez, s=80)
+
+fig2 = plt.figure()
+ax2 = fig2.add_subplot(111, projection='3d')
+plt.gca().invert_zaxis()
+
+# sacar_pred: lista que tiene sacar predecesores
+bloque_base_2 = (3, 3, 3)
+sacar_pred = [bloque for bloque in S[bloque_base_2]]
+for v in sacar_pred:
+    a = Arrow3D([(bloque_base_2[0]-1)*20, (v[0]-1)*20], [(bloque_base_2[1]-1)*20, (v[1]-1)*20],
+                [(bloque_base_2[2]-1)*15, (v[2]-1)*15], mutation_scale=20,
+                lw=3, arrowstyle="-|>", color="b")
+    ax2.add_artist(a)
+ax2.scatter(ejex, ejey, ejez, s=80)
 
 # LG para MB_sorted (i.e. que tiene bastantes menos nodos)
 
