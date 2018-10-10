29da492ce8a11a43399dfcfcf28835a9d93687b1
unknown <jpdonosomerlet@gmail.com>
2018-10-10 18:07:50 -0300

    Ahora S tiene los vecinos inmediatos. Falta implementarlo para el modelo de isa

--- Codigos/Pycharm/asignacion_arcos.py
+++ Codigos/Pycharm/asignacion_arcos.py
@@ -5,7 +5,6 @@ from mpl_toolkits.mplot3d import proj3d
 import numpy as np
 import math
 
-
 class Arrow3D(FancyArrowPatch):
 
     def __init__(self, xs, ys, zs, *args, **kwargs):
@@ -33,7 +32,7 @@ pos_z_MB = np.unique(ejez_MB.values)
 # para determinar el conjunto de aracos. Considerar
 # primero una cantidad mas pequeña de bloques, i.e. de niveles
 # en el eje z
-N = 4  # numero de ptos pos dimension
+N = 10  # numero de ptos pos dimension
 MB_sorted = MB.sort_values(by=['zcentre'], ascending=False)
 MB_sorted = MB_sorted.loc[MB_sorted['zcentre'] < pos_z_MB[N]]
 MB_sorted = MB_sorted.loc[MB_sorted['xcentre'] < pos_x_MB[N]]
@@ -79,43 +78,36 @@ theta_w = np.pi / 6
 theta_e = np.pi / 6
 arcos = []
 dicc_semiejes = {}
-for k in range(1, numz + 1):
-    for i in range(1, numx + 1):
-        for j in range(1, numy + 1):
-            for t in range(1, k):
-
-                dx1 = t * zdim / np.tan(theta_w)
-                dy1 = t * zdim / np.tan(theta_s)
-                dx2 = t * zdim / np.tan(theta_e)
-                dy2 = t * zdim / np.tan(theta_n)
-
-                m1 = dx1 / xdim
-                n1 = dy1 / ydim
-                m2 = dx2 / xdim
-                n2 = dy2 / ydim
-                print('###################################')
-                print('nivel t=', t, '.bloque base:', (i, j, k),
-                      '.semi-ejes: dx1 = %.2f, dy1 = %.2f, dx2 = %.2f, dy2 = %.2f' % (dx1, dy1, dx2, dy2))
-                print('Parametros de las cantidades de bloques: m1=%.2f, n1=%.2f, m2=%.2f, n2=%.2f' % (m1, n1, m2, n2))
-                for m in range(max(math.floor(i - m2), 1), min(numx, math.ceil(i + m1)) + 1):
-                    for n in range(max(math.floor(j - n2), 1), min(numy, math.ceil(j + n1)) + 1):
-                        print()
-                        a = xdim * (i - m)
-                        b = ydim * (j - n)
-                        print('*********** Distancia del bloque X_{%.2f,%.2f,%.f} ***********' % (m, n, k - t))
-                        print('a=%.2f, b=%.2f' % (a, b))
-                        a2 = a ** 2
-                        b2 = b ** 2
-                        if m >= i and n >= j:
-                            Value = a2 / (dx1 ** 2) + b2 / (dy1 ** 2)
-                        elif m >= i and n <= j:
-                            Value = a2 / (dx1 ** 2) + b2 / (dy2 ** 2)
-                        elif m <= i and n <= j:
-                            Value = a2 / (dx2 ** 2) + b2 / (dy2 ** 2)
-                        else:
-                            Value = a2 / (dx2 ** 2) + b2 / (dy1 ** 2)
-                        if Value <= 1:
-                            arcos.append([(i, j, k), (m, n, k - t)])
+# for k in range(1, numz + 1):
+#     for i in range(1, numx + 1):
+#         for j in range(1, numy + 1):
+#             for t in range(1, k):
+#
+#                 dx1 = t * zdim / np.tan(theta_w)
+#                 dy1 = t * zdim / np.tan(theta_s)
+#                 dx2 = t * zdim / np.tan(theta_e)
+#                 dy2 = t * zdim / np.tan(theta_n)
+#
+#                 m1 = dx1 / xdim
+#                 n1 = dy1 / ydim
+#                 m2 = dx2 / xdim
+#                 n2 = dy2 / ydim
+#                 for m in range(max(math.floor(i - m2), 1), min(numx, math.ceil(i + m1)) + 1):
+#                     for n in range(max(math.floor(j - n2), 1), min(numy, math.ceil(j + n1)) + 1):
+#                         a = xdim * (i - m)
+#                         b = ydim * (j - n)
+#                         a2 = a ** 2
+#                         b2 = b ** 2
+#                         if m >= i and n >= j:
+#                             Value = a2 / (dx1 ** 2) + b2 / (dy1 ** 2)
+#                         elif m >= i and n <= j:
+#                             Value = a2 / (dx1 ** 2) + b2 / (dy2 ** 2)
+#                         elif m <= i and n <= j:
+#                             Value = a2 / (dx2 ** 2) + b2 / (dy2 ** 2)
+#                         else:
+#                             Value = a2 / (dx2 ** 2) + b2 / (dy1 ** 2)
+#                         if Value <= 1:
+#                             arcos.append([(i, j, k), (m, n, k - t)])
 
 # Lo mismo de antes, pero solo predeccesor inmediato
 S = {}
@@ -123,15 +115,12 @@ S = {}
 for i in range(1, numx + 1):
     for j in range(1, numy + 1):
         S[(i, j, 1)] = []
-for k in range(1, numz + 1):
+for k in range(1,numz + 1):
+#for k in range(numz, 0, -1):
     for i in range(1, numx + 1):
         for j in range(1, numy + 1):
             lista_predecesores = []
             for t in range(1, k):
-                # dx1 = (k-t)*zdim/np.tan(theta_w)
-                # dy1 = (k-t)*zdim/np.tan(theta_s)
-                # dx2 = (k-t)*zdim/np.tan(theta_e)
-                # dy2 = (k-t)*zdim/np.tan(theta_n)
 
                 dx1 = t * zdim / np.tan(theta_w)
                 dy1 = t * zdim / np.tan(theta_s)
@@ -142,18 +131,19 @@ for k in range(1, numz + 1):
                 n1 = dy1 / ydim
                 m2 = dx2 / xdim
                 n2 = dy2 / ydim
-                print('###################################')
-                print('nivel t=', t, '.bloque base:', (i, j, k),
-                      '.semi-ejes: dx1 = %.2f, dy1 = %.2f, dx2 = %.2f, dy2 = %.2f' % (dx1, dy1, dx2, dy2))
-                print('Parametros de las cantidades de bloques: m1=%.2f, n1=%.2f, m2=%.2f, n2=%.2f' % (m1, n1, m2, n2))
                 for m in range(max(math.floor(i - m2), 1), min(numx, math.ceil(i + m1)) + 1):
                     for n in range(max(math.floor(j - n2), 1), min(numy, math.ceil(j + n1)) + 1):
-                        if m == i and n == j and t > 1:
-                            continue
+                        if k > 2 and t > 1:
+                            if n == j and (m+1,j,k-t+1) in S and (m,j,k-t) in S[(m+1,j,k-t+1)]:
+                                continue
+                            elif n == j and (m-1,j,k-t+1) in S and (m,j,k-t) in S[(m-1,j,k-t+1)]:
+                                continue
+                            elif m == i and (i,n+1,k-t+1) in S and (i,n,k-t) in S[(i,n+1,k-t+1)]:
+                                continue
+                            elif m == i and (i,n-1,k-t+1) in S and (i,n,k-t) in S[(i,n-1,k-t+1)]:
+                                continue
                         a = xdim * (i - m)
                         b = ydim * (j - n)
-                        print('\n*********** Distancia del bloque X_{%.2f,%.2f,%.f} ***********' % (m, n, k - t))
-                        print('a=%.2f, b=%.2f' % (a, b))
                         a2 = a ** 2
                         b2 = b ** 2
                         if m >= i and n >= j:
@@ -168,16 +158,37 @@ for k in range(1, numz + 1):
                             lista_predecesores.append((m, n, k - t))
             S[(i, j, k)] = lista_predecesores
 
-bloque_base_1 = (2, 3, 2)  # bloque base para plotear
-sacar = []
-for arco in arcos:
-    bloque_tail = arco[0]
-    bloque_head = arco[1]
-    if bloque_base_1 == bloque_tail:
-        sacar.append(bloque_head)
-print(sacar)
+
+
+#########################################################
+############ Graficar arcos de precedencia ##############
+#########################################################
+
+bloque_base_1 = (3, 4, 2)  # bloque base para plotear
+bloque_base_2 = (4, 2, 4)
+# sacar = []
+# for arco in arcos:
+#     bloque_tail = arco[0]
+#     bloque_head = arco[1]
+#     if bloque_base_1 == bloque_tail:
+#         sacar.append(bloque_head)
+# print(sacar)
 
 # graficar resultados de la asignacion
+# prueba: retirar bloques repetidos
+# for (p,q,z) in S[bloque_base_1]:
+#     if (p,q,z) in S[bloque_base_2]:
+#         S[bloque_base_2].remove((p,q,z))
+# ahora para todos los bloques
+for i in range(1,numx+1):
+    for j in range(1, numy + 1):
+        for k in range(1, numz + 1):
+            for (p,q,z) in [(i+1,j,k-1),(i-1,j,k-1),(i,j+1,k-1),(i,j-1,k-1),(i,j,k-1)]:
+                if (p,q,z) in S:
+                    for (w,t,h) in S[(p,q,z)]:
+                        if (w,t,h) in S[(i,j,k)]:
+                            S[(i,j,k)].remove((w,t,h))
+
 MB_grafico = MB_sorted[['xcentre', 'ycentre', 'zcentre']]-(min(MB_sorted[['xcentre']].values),
                                                            min(MB_sorted[['ycentre']].values),
                                                            min(MB_sorted[['zcentre']].values))
@@ -185,31 +196,35 @@ ejex = MB_grafico[['xcentre']]
 ejey = MB_grafico[['ycentre']]
 ejez = MB_grafico[['zcentre']]
 
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-plt.gca().invert_zaxis()
-
-for v in sacar:
-    a = Arrow3D([(bloque_base_1[0]-1)*20, (v[0]-1)*20], [(bloque_base_1[1]-1)*20, (v[1]-1)*20],
-                [(bloque_base_1[2]-1)*15, (v[2]-1)*15], mutation_scale=20,
-                lw=3, arrowstyle="-|>", color="r")
-    ax.add_artist(a)
-ax.scatter(ejex, ejey, ejez, s=80)
+# fig = plt.figure()
+# ax = fig.add_subplot(111, projection='3d')
+# plt.gca().invert_zaxis()
+#
+# #for v in sacar:
+# for v in S[bloque_base_1]:
+#     a = Arrow3D([(bloque_base_1[0]-1)*20, (v[0]-1)*20], [(bloque_base_1[1]-1)*20, (v[1]-1)*20],
+#                 [(bloque_base_1[2]-1)*15, (v[2]-1)*15], mutation_scale=20,
+#                 lw=3, arrowstyle="-|>", color="r")
+#     ax.add_artist(a)
+# ax.scatter(ejex, ejey, ejez, s=80)
 
 fig2 = plt.figure()
 ax2 = fig2.add_subplot(111, projection='3d')
 plt.gca().invert_zaxis()
 
 # sacar_pred: lista que tiene sacar predecesores
-bloque_base_2 = (3, 3, 3)
 sacar_pred = [bloque for bloque in S[bloque_base_2]]
 for v in sacar_pred:
     a = Arrow3D([(bloque_base_2[0]-1)*20, (v[0]-1)*20], [(bloque_base_2[1]-1)*20, (v[1]-1)*20],
                 [(bloque_base_2[2]-1)*15, (v[2]-1)*15], mutation_scale=20,
                 lw=3, arrowstyle="-|>", color="b")
     ax2.add_artist(a)
+for v in S[bloque_base_1]:
+    a = Arrow3D([(bloque_base_1[0]-1)*20, (v[0]-1)*20], [(bloque_base_1[1]-1)*20, (v[1]-1)*20],
+                [(bloque_base_1[2]-1)*15, (v[2]-1)*15], mutation_scale=20,
+                lw=3, arrowstyle="-|>", color="r")
+    ax2.add_artist(a)
 ax2.scatter(ejex, ejey, ejez, s=80)
 
 # LG para MB_sorted (i.e. que tiene bastantes menos nodos)
-
 plt.show()
