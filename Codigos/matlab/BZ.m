f66bd929bcbf5cbfb45e7d0b04d27500d655343e
jpmerlet <jpdonosomerlet@gmail.com>
2019-03-06 15:53:19 -0300

    ultimas actualizaciones para de los archivos antes de devolver

--- Codigos/matlab/BZ.m
+++ Codigos/matlab/BZ.m
@@ -1,7 +1,9 @@
 pcpsp_file = strcat('newman1','.pcpsp');
 fid = fopen(strcat('../minelib_inputs/',pcpsp_file));
-
-
-while 
-
+while True
+   linea =  fgetl(fid);
+   linea_cell = strsplit(linea, ' ');
+   switch linea_cell[0]
+       
+end
 fclose(fid);
